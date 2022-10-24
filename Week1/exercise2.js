35650734b1e87b929b2094c524c561edc3c352ef
HebaSalem <puupiiiii@gmail.com>
2022-10-25 01:26:27 +0200

    update exercise2.js databases-week1-homework

--- Week1/exercise2.js
+++ Week1/exercise2.js
@@ -1,4 +1,3 @@
-
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
@@ -8,16 +7,26 @@ const con = mysql.createConnection({
   database: "world"
 });
 
-con.connect(() => {
-  con.query("USE world");
-  con.query('SELECT name, population FROM country WHERE population > 8000000');
-  con.query('SELECT name FROM country WHERE name LIKE "%land%"');
-  con.query('SELECT name , population FROM city WHERE population BETWEEN 500000 AND 1000000');
-  con.query('SELECT name FROM country WHERE Continent = "Europe"');
-  con.query('SELECT * FROM country ORDER BY surfaceArea DESC');
-  con.query('SELECT name, population FROM city WHERE name = "Rotterdam"');
-  con.query('SELECT name, surfaceArea FROM country ORDER BY surfaceArea DESC LIMIT 10');
-  con.query('SELECT name, population FROM city ORDER BY population DESC LIMIT 10');
-  con.query('SELECT SUM(population) FROM country'); 
-  console.log('MySQL connected...');
+con.connect(err => {
+  if (err) throw err;
+  console.log('database connected...');
 });
+
+const queries = [
+  'SELECT name, population FROM country WHERE population > 8000000',
+  'SELECT name FROM country WHERE name LIKE "%land%"',
+  'SELECT name , population FROM city WHERE population BETWEEN 500000 AND 1000000',
+  'SELECT name FROM country WHERE Continent = "Europe"',
+  'SELECT * FROM country ORDER BY surfaceArea DESC',
+  'SELECT name, population FROM city WHERE name = "Rotterdam"',
+  'SELECT name, surfaceArea FROM country ORDER BY surfaceArea DESC LIMIT 10',
+  'SELECT name, population FROM city ORDER BY population DESC LIMIT 10',
+  'SELECT SUM(population) FROM country'
+]
+
+queries.forEach(query => {
+  con.query(query , (err , results) => {
+    if (err) throw err;
+    results.forEach(result => console.log(result))
+  })
+})
