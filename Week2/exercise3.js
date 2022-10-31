52031cbd9111e2f36345027d5ae09e4496048481
HebaSalem <puupiiiii@gmail.com>
2022-10-31 21:01:15 +0100

    exercise3.js

--- Week2/exercise3.js
+++ Week2/exercise3.js
@@ -8,26 +8,32 @@ const con = mysql.createConnection({
 });
 
 
-con.connect(() => {
-  
-  con.query(`
-    SELECT t1.author_name AS Author, t2.author_name AS Mentor
-      FROM authors AS t1 
-      INNER JOIN authors AS t2
-      ON t1.mentor = t2.author_id;
-  `);       
-  
-  con.query(`
-      SELECT *, paper_title 
-        FROM authors 
-        LEFT JOIN author_paper
-        ON author_paper.author_id = authors.author_id
-        LEFT JOIN research_papers
-        ON research_papers.paper_id = author_paper.paper_id;
-  `);
+con.connect( err => {
+  if(err) throw err
+  console.log('database connected...')
+})
 
+const queries = [
+  `SELECT t1.author_name AS Author, t2.author_name AS Mentor
+    FROM authors AS t1 
+    INNER JOIN authors AS t2
+    ON t1.mentor = t2.author_id`,
+    
+  `SELECT *, paper_title 
+    FROM authors 
+    LEFT JOIN author_paper
+    ON author_paper.author_id = authors.author_id
+    LEFT JOIN research_papers
+    ON research_papers.paper_id = author_paper.paper_id`
+]
+  
+queries.forEach(query => {
+  con.query(query , (err, results) => {
+    if(err) throw err
+    console.table(results)
+  })
+})  
 
-});
 
 
 
