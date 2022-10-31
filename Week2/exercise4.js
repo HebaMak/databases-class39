2c5c144292365dd388353ec599a15e1ab701ab51
HebaSalem <puupiiiii@gmail.com>
2022-10-31 21:09:22 +0100

    edit exercise4.js

--- Week2/exercise4.js
+++ Week2/exercise4.js
@@ -8,48 +8,49 @@ const con = mysql.createConnection({
 });
 
 
-con.connect(() => {
-  
-  con.query(`
-    SELECT research_papers.paper_title , COUNT(author_name)
+con.connect(err => {
+  if(err) throw err
+  console.log('database connected...')
+})
+
+const queries = [
+  `SELECT research_papers.paper_title , COUNT(author_name)
       FROM authors
       LEFT JOIN author_paper 
       ON author_paper.author_id = authors.author_id
       LEFT JOIN research_papers
       ON research_papers.paper_id = author_paper.paper_id
-      GROUP BY author_name;
-  `);
-
-  con.query(`
-    SELECT gender, COUNT(paper_title) FROM authors
+      GROUP BY author_name`,
+  
+  `SELECT gender, COUNT(paper_title) FROM authors
       JOIN author_paper
       ON author_paper.author_id = authors.author_id
       JOIN research_papers
       ON research_papers.paper_id = author_paper.paper_id
-      WHERE authors.gender = 'f';
-  `);
-
-  con.query(`
-    SELECT  university, AVG(h_index) FROM authors
-      GROUP BY university;
-  `);
-
-  con.query(`
-    SELECT university, COUNT(paper_title) FROM authors
+      WHERE authors.gender = 'f'`,
+    
+  `SELECT  university, AVG(h_index) FROM authors
+      GROUP BY university`,
+  
+  `SELECT university, COUNT(paper_title) FROM authors
       JOIN author_paper
       ON author_paper.author_id = authors.author_id
       JOIN research_papers
       ON research_papers.paper_id = author_paper.paper_id
-      GROUP BY university
-  `);
-
-  con.query(`
-    SELECT author_name, university, MAX(h_index), MIN(h_index)
+      GROUP BY university`,
+    
+  `SELECT author_name, university, MAX(h_index), MIN(h_index)
       FROM authors
-      GROUP BY university;
-  `);
+      GROUP BY university`
+]
 
-});
+
+queries.forEach(query => {
+  con.query(query , (err, results)=> {
+    if(err) throw err
+    console.table(results)
+  })
+})
 
 
 
